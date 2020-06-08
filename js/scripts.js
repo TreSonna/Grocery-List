eff049f68915921f5c97f1a0d88b6716d48591b8
Michael Watts <Wattsjmichael@gmail.com>
2020-06-08 16:09:41 -0700

    Change camel casing
    Co-authored-by: Vanessa Guerrero <vamarilug@gmail.com>
    co-authored-by: Tre Sonna <tresonna@gmail.com>

--- js/scripts.js
+++ js/scripts.js
@@ -2,11 +2,11 @@ $(document).ready(function(){
 $("#groceryList").sumbit(function(event)){
     event.prevent.default();
     let item=[".item1",".item2",".item3",".item4",".item5"];
-    let itemname=[]
-    itemname.push($("input#item1").val());
-    itemname.push($("input#item2").val());
-    itemname.push($("input#item3").val());
-    itemname.push($("input#item4").val());
-    itemname.push($("input#item5").val());
+    let itemName=[]
+    itemName.push($("input#item1").val());
+    itemName.push($("input#item2").val());
+    itemName.push($("input#item3").val());
+    itemName.push($("input#item4").val());
+    itemName.push($("input#item5").val());
 
-    itemname.forEach(function(element, index)
+    itemName.forEach(function(element, index)
